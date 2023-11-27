b05ffa8fe8d09e86641f0fe2be93b8b7def41681
Mahardika Kharisma Ajie <mahardikakharisma@gmail.com>
2023-11-27 13:41:37 +0700

    check all winning condition

--- js/index.js
+++ js/index.js
@@ -4,19 +4,163 @@ let gridSize = 3;
 let winningCondition = 3;
 let winningPattern = new Array();
 
+function checkStraightPattern(x, y, val, type, isReverse) {
+  let ref = y;
+
+  if (type === "vertical") {
+    ref = x;
+  }
+
+  let valChecker = true;
+
+  if (isReverse) {
+    for (let i = ref; i >= ref - (winningCondition - 1); i--) {
+      let source = squareValue[x][i];
+      if (type === "vertical") {
+        source = squareValue[i][y];
+      }
+      valChecker = valChecker && source === val;
+    }
+  } else {
+    for (let i = ref; i <= ref + (winningCondition - 1); i++) {
+      let source = squareValue[x][i];
+      if (type === "vertical") {
+        source = squareValue[i][y];
+      }
+      valChecker = valChecker && source === val;
+    }
+  }
+
+  if (valChecker) {
+    return true;
+  }
+}
+
+function checkDiagonalPattern(x, y, val, direction) {
+  let valChecker = true;
+
+  switch (direction) {
+    case "bottom-right":
+      for (let i = x; i <= x + (winningCondition - 1); i++) {
+        valChecker = valChecker && squareValue[i][i] === val;
+      }
+      break;
+
+    case "top-left":
+      let j = 0;
+      for (let i = x; i >= x - (winningCondition - 1); i--) {
+        valChecker = valChecker && squareValue[i][y - j] === val;
+        j++;
+      }
+      break;
+
+    case "bottom-left":
+      let k = 0;
+      for (let i = x; i <= x + (winningCondition - 1); i++) {
+        valChecker = valChecker && squareValue[i][y - k] === val;
+        k++;
+      }
+      break;
+
+    case "top-right":
+      let l = 0;
+      for (let i = x; i >= x - (winningCondition - 1); i--) {
+        valChecker = valChecker && squareValue[i][y + l] === val;
+        l++;
+      }
+      break;
+
+    default:
+      break;
+  }
+
+  if (valChecker) {
+    return true;
+  }
+}
+
+function checkWinningCondition(x, y, val) {
+  // Check horizontal to left
+  if (y >= winningCondition - 1) {
+    if (checkStraightPattern(x, y, val, "horizontal", true)) {
+      return true;
+    }
+  }
+
+  if (y + (winningCondition - 1) < gridSize) {
+    // Check horizontal to right
+    if (checkStraightPattern(x, y, val, "horizontal", false)) {
+      return true;
+    }
+
+    if (x - (winningCondition - 1) >= 0) {
+      // Check diagonal to top right
+      if (checkDiagonalPattern(x, y, val, "top-right")) {
+        return true;
+      }
+    }
+  }
+
+  if (x >= winningCondition - 1) {
+    // Check vertical to top
+    if (checkStraightPattern(x, y, val, "vertical", true)) {
+      return true;
+    }
+
+    // Check diagonal to top left
+    if (checkDiagonalPattern(x, y, val, "top-left")) {
+      return true;
+    }
+  }
+
+  if (x + (winningCondition - 1) < gridSize) {
+    // Check vertical to bottom
+    if (checkStraightPattern(x, y, val, "vertical", false)) {
+      return true;
+    }
+
+    // Check diagonal to bottom right
+    if (checkDiagonalPattern(x, y, val, "bottom-right")) {
+      return true;
+    }
+
+    if (y - (winningCondition - 1) >= 0) {
+      // Check diagonal to bottom left
+      if (checkDiagonalPattern(x, y, val, "bottom-left")) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 function setValue(row, col) {
+  const symbolVal = isX ? "x" : "o";
   const getSquare = document.getElementById(`square-${row}-${col}`);
   getSquare.innerHTML = isX ? "X" : "O";
   getSquare.setAttribute("disabled", true);
-  squareValue[row][col] = isX ? "x" : "o";
-  isX = !isX;
+  squareValue[row][col] = symbolVal;
 
-  document.getElementById("info").innerHTML = `Current player : ${
-    isX ? "X" : "O"
-  }`;
+  if (checkWinningCondition(row, col, symbolVal)) {
+    const getSquareBtn = document.getElementsByClassName("squarebox");
+    for (let i = 0; i < getSquareBtn.length; i++) {
+      getSquareBtn[i].setAttribute("disabled", true);
+    }
 
-  // Cek winning pattern
-  // If win show modal with reset button
+    document.getElementById("info").innerHTML = `${
+      isX ? "X" : "O"
+    } is the winner !`;
+    setTimeout(() => {
+      alert(`The winner is ${symbolVal} !`);
+    }, 100);
+  } else {
+    isX = !isX;
+
+    document.getElementById("info").innerHTML = `Current player : ${
+      isX ? "X" : "O"
+    }`;
+  }
 }
 
 (() => {
@@ -35,6 +179,13 @@ function setValue(row, col) {
       } else {
         gridSize = document.getElementById("gridSize").value;
         winningCondition = document.getElementById("winningCondition").value;
+        isX = true;
+        document.getElementById("info").innerHTML = "Current player : X";
+
+        document.getElementById("info-parent").style.display = "block";
+
+        const squareContainer = document.getElementById("square-container");
+        squareContainer.innerHTML = "";
 
         let tmpRowVal = new Array();
         for (let i = 0; i < gridSize; i++) {
@@ -59,7 +210,6 @@ function setValue(row, col) {
 
           tmpRowVal.push(tmpColVal);
 
-          const squareContainer = document.getElementById("square-container");
           squareContainer.appendChild(squareRow);
         }
         squareValue = tmpRowVal;
